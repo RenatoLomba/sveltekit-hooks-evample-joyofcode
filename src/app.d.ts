dff516afc895db45dc47add5074496d29520d1e2
RenatoLomba <rntlomba@gmail.com>
2023-06-09 21:26:10 -0300

    transforming html

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,13 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user?: {
+				name: string;
+				email: string;
+			};
+			locale: string;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,15 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const locale = event.cookies.get('locale') ?? 'en';
+
 	event.locals.user = {
 		name: 'Renato',
 		email: 'rntlomba@gmail.com',
 	};
+	event.locals.locale = locale;
 
-	return resolve(event);
+	return resolve(event, {
+		transformPageChunk: ({ html }) => html.replace('%lang%', locale),
+	});
 };
